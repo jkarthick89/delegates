110ef786dbad12034d30d3cbe9e2324306d2a3cc
JK <jkarthick89@gmail.com>
2016-02-22 16:58:33 +0530

    My First Git push

---
+++ Delegates/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Delegates
+{
+    class Program 
+    {
+        
+        static void Main(string[] args)
+        {
+            Maths oMaths = new Maths();
+           int a = oMaths.GetPointer(5).Invoke(4, 4);
+        }
+
+       
+    }
+
+    class Maths
+    {
+        public delegate int MathOperation(int i, int j);
+        
+
+        public MathOperation GetPointer(int i)
+        {
+            MathOperation oDelegete = null;
+            if (i == 1)
+                oDelegete = Add;
+            else
+                oDelegete = Sub;
+
+            return oDelegete;
+        }
+
+        int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
